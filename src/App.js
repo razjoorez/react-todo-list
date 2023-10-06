0354ac41a5d3d5a205ee3060078efba4e9142a6f
Reza Razjoo <reza.razjoo@yahoo.com>
2023-10-06 21:02:00 +0100

    css for todo input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+ const [newItem, setNewItem]=  useState("");
+ const [todos, setTodos] = useState([{id: Math.random(), name: 'new todo do be doo', completed: false},
+         {id: Math.random(), name: 'todo 2', completed: false},
+         {id: Math.random(), name: 'todo3 ', completed: false}]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit ingggg<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='container'>
+        <h1>Todo List</h1>
+    <form>
+      <label htmlFor='item'>Add Item</label>
+      <input id='item' placeholder='Add a new todo item...' value={newItem} onChange={(e)=> setNewItem(e.target.value)}></input>
+      
+      <button >Add</button>
+      <ul>
+     {todos.map((todo) => {
+      return(
+        <li>
+          <label>
+
+          {todo.name}
+          </label>
+         
+          </li>
+      )
+     })}
+     
+    </ul>
+    </form>
+  
+   
     </div>
   );
 }
+
 
 export default App;
