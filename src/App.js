84bd2bfbba0d958281dd1e543198af865d18c457
Reza Razjoo <reza.razjoo@yahoo.com>
2023-10-07 17:42:08 +0100

    ui for table done

--- src/App.js
+++ src/App.js
@@ -17,21 +17,33 @@ function App() {
       <input id='item' placeholder='Add a new todo item...' value={newItem} onChange={(e)=> setNewItem(e.target.value)}></input>
       
       <button >Add</button>
-      <ul>
-     {todos.map((todo) => {
-      return(
-        <li>
-          <label>
-
-          {todo.name}
-          </label>
-         
-          </li>
-      )
-     })}
-     
-    </ul>
+      
     </form>
+
+    
+      <div className="table-container">
+    
+    <div className="table-row">
+      <div className="row-item">
+      Task Name
+    </div>
+     <div className="row-item">
+      Task Name
+    </div>
+    </div>
+    
+    <div className="table-row">
+       <div className="row-item">
+      Task Name
+    </div>
+       <div id="container">
+    <button type="button">Button     1</button>
+    <button type="button">Button 2</button>
+</div>
+    </div>
+   
+  </div>
+
   
    
     </div>
